8dd11702cbdfc71fbcff386fe15220e082720aa4
camnh <hongcam941997@gmail.com>
2018-06-13 17:00:12 +0700

    CRUD Photo

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ group :development, :test do
   gem 'byebug', platforms: %i(mri mingw x64_mingw)
   gem 'capybara', '~> 2.13'
   gem 'selenium-webdriver'
+  gem 'pry'
 end
 
 group :development do
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -5,4 +5,42 @@ class Photo < ApplicationRecord
   has_many :rates
   validates :image_url, presence: true
   validates :title, presence: true
+  rails_admin do
+    list do
+      field :title do
+        label 'Title'
+      end
+      field :description do
+        label 'Description'
+      end
+      field :image_url do
+        label 'Image_url'
+      end
+      field :user_id do
+        label 'User'
+        formatted_value do
+          user = User.find_by(id: value)
+          user.nil? ? 'Empty' : user.name
+        end
+      end
+    end
+
+    edit do
+      field :title do
+        label 'Title'
+      end
+      field :description do
+        label 'Description'
+      end
+      field :image_url do
+        label 'Image_url'
+      end
+      field :user_id, :enum do
+        label "Users"
+        enum do
+          User.all.map { |u| [u.name, u.id] }
+        end
+      end
+    end
+  end
 end
