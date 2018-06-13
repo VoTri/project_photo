e20117f1165d31088a852b4f35fa311e66732583
camnh <hongcam941997@gmail.com>
2018-06-13 17:00:12 +0700

    CRUD Photo

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -5,4 +5,23 @@ class Photo < ApplicationRecord
   has_many :rates
   validates :image_url, presence: true
   validates :title, presence: true
+  rails_admin do
+    list do
+      field :title do
+        label 'Title'
+      end
+      field :description do
+        label 'Description'
+      end
+      field :image_url do
+        label 'Image_url'
+      end
+      field :user_id do
+        label 'User'
+         formatted_value do
+          Users.find_by(id: value).name
+        end
+      end
+    end
+  end
 end
