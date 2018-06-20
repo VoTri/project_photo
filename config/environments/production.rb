11e64a5568ff2df768bafec0a9dd0ca2b56b01ec
camnh <hongcam941997@gmail.com>
2018-06-20 09:06:37 +0700

    ckeditor

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,13 +25,10 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  # config.assets.css_compressor = :Strict-Transport-Security
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
-  config.assets.precompile += Ckeditor.assets
-  config.assets.precompile += %w(ckeditor/* )
-  config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
