ee8d0a1cb9fc27228ae7e2b2ea344760cec74566
camnh <hongcam941997@gmail.com>
2018-06-20 09:26:18 +0700

    image

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Demo
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.assets.precompile += Ckeditor.assets
+    config.assets.precompile += %w( ckeditor/* )
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
