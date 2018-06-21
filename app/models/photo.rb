b076be8aae93e8e112b098464f3c3fb6eb648ccb
camnh <hongcam941997@gmail.com>
2018-06-21 08:16:13 +0700

    fix

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,7 +2,7 @@ class Photo < ApplicationRecord
   belongs_to :user
   has_many :comments
   has_many :cate_photos
-  # has_many :rates
+  has_many :rates
   validates :image_url, presence: true
   validates :title, presence: true
   ratyrate_rateable "price"
