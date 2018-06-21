75a0175e2f123913ba5f22eca455376afa141d71
camnh <hongcam941997@gmail.com>
2018-06-21 10:34:41 +0700

    rate

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,6 +9,7 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.precompile += %w( ckeditor/*)
 Rails.application.config.assets.precompile += %w( ratyrate.js )
+Rails.application.config.assets.precompile += %w( *.js ^[^_]*.css *.css.erb )
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
