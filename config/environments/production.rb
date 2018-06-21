bd4a381e7df4273dba5b48f1a85d3e098dc627d6
camnh <hongcam941997@gmail.com>
2018-06-21 10:55:52 +0700

    adasdsa

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
+  config.assets.debug = true
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
@@ -28,9 +29,8 @@ Rails.application.configure do
   # config.assets.css_compressor = :Strict-Transport-Security
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
-  config.assets.debug = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
