5261d74708f5cb82e8549fed194f2ba2ac77dae6
camnh <hongcam941997@gmail.com>
2018-06-21 09:14:41 +0700

    fix gem

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,5 @@ module Demo
     # config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
 
     config.assets.precompile += Ckeditor.assets
-    config.assets.precompile += %w( ckeditor/* )
-    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
   end
 end
