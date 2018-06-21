ca269cf73b3ae77d54f4ef97a3f6cbbec14d3f19
camnh <hongcam941997@gmail.com>
2018-06-21 08:34:59 +0700

    fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap.min
+
 //= require jquery
 //= require rails-ujs
+//= require bootstrap.min
 //= require bootstrap-sprockets
 //= require popper.min
 //= require plugins
